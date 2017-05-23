4b33201f28c58f6f95cfc81d8af3739519f7e565
leonyh7 <leo_nyh@sina.com>
2017-05-23 11:07:30 +0800

    搜索

--- src/views/query/query.vue
+++ src/views/query/query.vue
@@ -1,6 +1,79 @@
 <template>
-  <div>query</div>
+  <div>
+    <search
+    @result-click="resultClick"
+    @on-change="getResult"
+    :results="results"
+    v-model="value"
+    @on-focus="onFocus"
+    @on-cancel="onCancel"
+    @on-submit="onSubmit"
+    ref="search"></search>
+    <div class="apin-btn-search">
+      <x-button @click.native="setFocus" type="primary">确认</x-button>
+    </div>
+  </div>
 </template>
 
 <script>
+import { Search, Group, Cell, XButton } from 'vux'
+
+export default {
+  components: {
+    Search,
+    Group,
+    Cell,
+    XButton
+  },
+  methods: {
+    setFocus () {
+      this.$refs.search.setFocus()
+    },
+    resultClick (item) {
+      window.alert('you click the result item: ' + JSON.stringify(item))
+    },
+    getResult (val) {
+      this.results = val ? getResult(this.value) : []
+    },
+    onSubmit () {
+      this.$vux.toast.show({
+        type: 'text',
+        position: 'top',
+        text: 'on submit'
+      })
+    },
+    onFocus () {
+      console.log('on focus')
+    },
+    onCancel () {
+      console.log('on cancel')
+    }
+  },
+  data () {
+    return {
+      results: [],
+      value: 'test'
+    }
+  }
+}
+
+function getResult (val) {
+  let rs = []
+  for (let i = 0; i < 8; i++) {
+    rs.push({
+      title: `${val} result: ${i + 1} `,
+      other: i
+    })
+  }
+  return rs
+}
 </script>
+
+<style>
+.apin-btn-search {
+  padding: 15px;
+}
+.vux-search-box.vux-search-fixed + .apin-btn-search {
+  margin-top: 44px;
+}
+</style>
